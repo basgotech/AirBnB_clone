e1ae4fe909cb15abecbd7ea795adbf38a1a929ef
fraolketema <138703893+fraolketema@users.noreply.github.com>
2024-02-13 21:09:35 +0300

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Test BaseModel for expected behavior and documentation"""
+"""Test BaseModel for expected behavior and documentations"""
 from datetime import datetime
 import inspect
 import pep8 as pycodestyle
